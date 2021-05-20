a1ae4dbdd80ce538900550541b3793ff15274aca
Abhishek Tilwar <abhishektilwar@gmail.com>
2021-05-20 19:11:36 +0530

    Update min-max.cpp

--- Array/min-max.cpp
+++ Array/min-max.cpp
@@ -1,3 +1,26 @@
+//efficient code- complexity O(n)
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    int n, arr[10];
+    cin>>n;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int maxno=INT_MIN;
+    int minno=INT_MAX;
+
+    for(int i=0; i<n; i++){
+        minno=min(minno,arr[i]);
+        maxno=max(maxno,arr[i]);
+    }
+    cout<<maxno<<" "<<minno;
+}
+
+
 // less efficient code- complexity O(n^2)
 #include <iostream>
 using namespace std;
@@ -26,6 +49,4 @@ int main()
     sort(arr,n);
     cout<<"Minimum no. "<<arr[0]<<endl;
     cout<<"Maximum no. "<<arr[n-1];
-    
-return 0;
 }
