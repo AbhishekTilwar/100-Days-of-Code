ab9539a7cae5c8f26aacbdd082cbb3360318db91
Abhishek Tilwar <abhishektilwar@gmail.com>
2021-05-20 18:39:08 +0530

    Create min-max.cpp

---
+++ Array/min-max.cpp
@@ -0,0 +1,31 @@
+// less efficient code- complexity O(n^2)
+#include <iostream>
+using namespace std;
+
+int sort(int arr[], int n){
+    for(int i=0; i<n-1; ++i){
+        for(int j=0; j<n-i-1; ++j){
+            if(arr[j]>arr[j+1])
+            {
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, arr[10];
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    sort(arr,n);
+    cout<<"Minimum no. "<<arr[0]<<endl;
+    cout<<"Maximum no. "<<arr[n-1];
+    
+return 0;
+}
